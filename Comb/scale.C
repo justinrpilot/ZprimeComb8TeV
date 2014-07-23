a2daea14c58cd6751f612f2259d141867c17e82b
Roman Kogler <roman.kogler@cern.ch>
2014-07-23 15:01:10 +0200

    combined jec and subjec for HTT analysis

--- Comb/scale.C
+++ Comb/scale.C
@@ -1,5 +1,6 @@
 void scale(TString inname = "CMSTT_zpn.root")
 {
+  //inname = "HEPTT_zpn.root";
   //inname = "dilep_zpn.root";
   //inname = "ljets_all.root";
 
@@ -44,10 +45,25 @@ void scale(TString inname = "CMSTT_zpn.root")
     TObjArray* tokname = hname.Tokenize("#");
     TString process = ((TObjString*) tokname->At(1))->GetString();
 
-    // write histogram to new file
-    outfile->cd();
-    hist->Write();
-    
+    // write histogram to new file, with one exception: subjec for HEPTT analysis
+    // is combined with jec
+    Bool_t write = true;
+    if (inname.Contains("HEPTT")){
+      if (tokname->GetEntries()==4){
+	TString cat = ((TObjString*) tokname->At(0))->GetString();
+	TString sys = ((TObjString*) tokname->At(2))->GetString();
+	TString var = ((TObjString*) tokname->At(3))->GetString();      
+	if (sys == "subjec"){
+	  write = false;
+	}
+      }
+    }
+
+    if (write){
+      outfile->cd();
+      hist->Write();
+    }
+	  
     // never change DATA!
     if (process == "DATA") continue;
 
@@ -78,9 +94,26 @@ void scale(TString inname = "CMSTT_zpn.root")
       }
     }
 
+    // correlate jec and subjec for HTT analysis
+    if (inname.Contains("HEPTT")){
+
+      if (tokname->GetEntries()==4){
+	TString cat = ((TObjString*) tokname->At(0))->GetString();
+	TString sys = ((TObjString*) tokname->At(2))->GetString();
+	TString var = ((TObjString*) tokname->At(3))->GetString();      
+	if (sys == "jec"){
+	  cout << "\ngot jec for process " << process << " and category " << cat << endl;
+	  CombineJECs(hist, cat, process, sys, var, file);	
+	}
+      }
+
+    }
+
     // write histogram to new file
-    outfile->cd();
-    hist->Write("", TObject::kOverwrite);
+    if (write){
+      outfile->cd();
+      hist->Write("", TObject::kOverwrite);
+    }
 
   }
  
@@ -88,6 +121,30 @@ void scale(TString inname = "CMSTT_zpn.root")
   outfile->Write();
   outfile->Close();
 
+
+
+}
+
+void CombineJECs(TH1* hist, TString cat, TString process, TString sys, TString var, TFile* file)
+{
+
+  // combine jec and subjec in one uncertainty: n + (jec - n) + (subjec - n) = jec + subjec - n
+  // where n stands for the nominal sample
+
+  // get the subjec hist:
+  TString sjecn = cat + "__" + process + "__" + "subjec" + "__" + var;
+  TString nomname = cat + "__" + process;
+
+  TH1F* hsjec = (TH1F*) file->Get(sjecn);
+  TH1F* hnominal = (TH1F*) file->Get(nomname);
+
+  //  cout << "added: " << hist->GetName() << " + " << hsjec->GetName() << " - " << hnominal->GetName() << endl;
+
+  hist->Add(hsjec, 1.);
+  hist->Add(hnominal, -1.);
+
+  hsjec->Delete();
+  hnominal->Delete();
 
 
 }
