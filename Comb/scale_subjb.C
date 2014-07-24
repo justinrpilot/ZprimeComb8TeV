36ba88ae27f32b176aabde44db82215d785fb00b
Roman Kogler <roman.kogler@cern.ch>
2014-07-24 11:42:50 +0200

    fixed a NAN bug, removed qcd_misERR from scaled files

--- Comb/scale_subjb.C
+++ Comb/scale_subjb.C
@@ -27,6 +27,18 @@ void scale_subjb(TString inname = "CMSTT_zpn.root")
     TObjArray* tokname = hname.Tokenize("#");
     TString process = ((TObjString*) tokname->At(1))->GetString();
 
+    TString cat;
+    TString sys;
+    TString var;
+
+    if (tokname->GetEntries()==4){
+      cat = ((TObjString*) tokname->At(0))->GetString();
+      sys = ((TObjString*) tokname->At(2))->GetString();
+      var = ((TObjString*) tokname->At(3))->GetString();
+    }
+
+    if (sys == "misErr") continue;
+
     // write histogram to new file
     outfile->cd();
     hist->Write("", TObject::kOverwrite);
@@ -75,10 +87,18 @@ TH1F* GetVariation(TString cat, TString process, TString sys, TString var, Doubl
     if ( !(hnominal->GetBinContent(i)>0) ) continue;
 
     // get difference and scale it with the nuisance parameter
-    double diff = hsys->GetBinContent(i) - hnominal->GetBinContent(i);
+    double vsys = hsys->GetBinContent(i);
+    if (vsys<0) vsys = 0;
+    double diff = vsys - hnominal->GetBinContent(i);
     double rdiff = diff/hnominal->GetBinContent(i);
-    double sf = TMath::Exp( nu * TMath::Log(1+rdiff) );
-    //cout << "diff = " << diff << " sd = " << sf << endl;
+    double sf = 0.;
+    if (vsys>0){
+      sf = TMath::Exp( nu * TMath::Log(1+rdiff) );
+    }
+    //if (cat.Contains("btag4") && process.Contains("1250")){            
+    //  cout << "\ndiff = " << diff << " sd = " << sf << " log = " << TMath::Log(1+rdiff) << " rdiff = " << rdiff << endl;
+    //  cout << "hsys = " << hsys->GetBinContent(i) << " nominal = " << hnominal->GetBinContent(i) << endl;
+    //}
 
     hvar->SetBinContent(i, sf);
   }
@@ -115,6 +135,7 @@ void ScaleProcess(TString cat, TString process, TH1F* varh, TFile* file, TFile* 
 	
 	// scale the histogram content 
 	double val = hist->GetBinContent(i) * varh->GetBinContent(i);
+	if (val<0) val = 0.;
 	
 	hist->SetBinContent(i, val);
       }
