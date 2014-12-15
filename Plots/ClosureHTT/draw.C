c63301ed9c166b8bc93af9385b1b61b87201611a
rkogler <roman.kogler@cern.ch>
2014-12-15 17:40:08 +0100

    update of figures

--- Plots/ClosureHTT/draw.C
+++ Plots/ClosureHTT/draw.C
@@ -7,6 +7,8 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   gStyle -> SetPadTickX(1);
   gStyle -> SetPadTickY(1);
 
+  gStyle->SetEndErrorSize(0);
+
   gStyle->SetPadBorderMode(0);
   gStyle->SetPadColor(kWhite);
   gStyle->SetPadGridX(false);
@@ -77,18 +79,20 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   data_eff->SetMarkerStyle(20);
   data_eff->SetMarkerColor(kBlack);
   data_eff->SetLineColor(kBlack);
-  data_eff->SetLineWidth(1);
-  data_eff->SetMarkerSize(0.5);
-  data_eff->Draw("E1");
+  data_eff->SetLineWidth(2);
+  data_eff->SetMarkerSize(0.8);
+  data_eff->Draw("E1 X0");
   
 
   double x,y;
 
-  mc_eff->SetMarkerStyle(25);
-  mc_eff->SetMarkerColor(kRed+1);
-  mc_eff->SetLineColor(kRed+1);
+  Int_t fcol = kOrange - 3;
+  //mc_eff->SetMarkerStyle(25);
+  mc_eff->SetMarkerColor(fcol);
+  mc_eff->SetLineColor(fcol);
   mc_eff->SetLineWidth(1);
-  mc_eff->SetMarkerSize(0.5);   
+  mc_eff->SetFillColor(fcol);
+  //mc_eff->SetMarkerSize(0.5);   
 
  
   TH1F* painter = data_eff->Clone();
@@ -115,8 +119,29 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   //l->SetLineColor(kBlue);
   //l->Draw();
 
-  mc_eff->Draw("E1same");
-  data_eff->Draw("E1same");
+  //mc_eff->Draw("E1same");
+  mc_eff->Draw("HISTsame");
+
+  // draw an error for the background prediction
+  TGraphAsymmErrors* eAsym = new TGraphAsymmErrors();
+  for (Int_t i=1; i<mc_eff->GetNbinsX()+1; ++i){
+    Double_t sys = 0;
+    Double_t stat = mc_eff->GetBinError(i);
+    eAsym -> SetPoint(i, mc_eff->GetXaxis()->GetBinCenter(i), mc_eff->GetBinContent(i));
+    Double_t ex_low = (mc_eff->GetXaxis()->GetBinCenter(i)) - (mc_eff->GetXaxis()->GetBinLowEdge(i));
+    Double_t ex_up = (mc_eff->GetXaxis()->GetBinUpEdge(i))-mc_eff->GetXaxis()->GetBinCenter(i);
+    eAsym -> SetPointError(i, ex_low, ex_up, stat, stat);
+  }
+  Int_t LightGray = TColor::GetColor( "#aaaaaa" );
+  //h->SetFillColor(kGray+2);
+  eAsym->SetFillColor(LightGray);
+  eAsym->SetLineWidth(1);
+  eAsym->SetFillStyle(3245);
+  eAsym->Draw("E2 same");
+
+  // draw the data on top of the error
+  data_eff->Draw("E1 same");
+
 
   m_rp1_top->SetLogy(1);
 
@@ -152,7 +177,8 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   text3->SetY(0.73);
   //text3->Draw();
 
-  TLegend *leg = new TLegend(0.4,0.675,0.73,0.845, NULL,"brNDC");
+  //TLegend *leg = new TLegend(0.4,0.675,0.73,0.845, NULL,"brNDC");
+  TLegend *leg = new TLegend(0.4,0.65,0.73,0.845, NULL,"brNDC");
   leg->SetFillColor(0);
   leg->SetLineColor(1);
   leg->SetBorderSize(0);
@@ -161,7 +187,7 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   if (lohiDY == 0) leg->AddEntry(totalH, "0+1+2 b-tags, H_{T}>800 GeV", "");
   else leg->AddEntry(totalH, "0+1+2 b-tags, H_{T}<800 GeV", "");
   TLegendEntry* dleg = leg->AddEntry(data_eff, "Selected QCD", "lpe");
-  TLegendEntry* dmc = leg->AddEntry(mc_eff, "Predicted QCD", "lpe");
+  TLegendEntry* dmc = leg->AddEntry(mc_eff, "Predicted QCD", "f");
 
   dleg->SetTextFont(42);
   dmc->SetTextFont(42);
@@ -182,7 +208,7 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   ratio->SetLineWidth(1);
   ratio->SetLineColor(kBlack);
   ratio->SetMarkerSize(1.0);
-  ratio->Draw("E1");
+  ratio->Draw("E0");
 
   TLine* l = new TLine(250, 1, 3000, 1);
   l->SetLineStyle(kDashed);
@@ -191,13 +217,11 @@ void draw(TString fname = "qcdClosure_lowDY.root", int lohiDY = 0)
   TH1F* rpainter = data_eff->Clone();
   rpainter->Divide(mc_eff);
 
-  ratio->GetXaxis()->SetRangeUser(0, 0.5);
-
   ratio->GetXaxis()->SetTitle("Dijet Invariant Mass [GeV]");
   ratio->GetYaxis()->SetTitle("Sel./Pred.");
   ratio->SetTitle("");
 
-  ratio->GetYaxis()->SetRangeUser(0.3, 2.3);
+  ratio->GetYaxis()->SetRangeUser(0.3, 1.7);
   ratio->GetXaxis()->SetRangeUser(250, 3000);
   ratio->SetMarkerSize(0.7);
 
